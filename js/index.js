d02ed556ec7ef4f37e9215987efdcc2ecda4e802
Henrique <henrique.kishida@hotmail.com>
2023-03-20 15:58:15 -0300

    feat: navbar scroll function and picture slider style: responsiveness

--- js/index.js
+++ js/index.js
@@ -1,13 +1,37 @@
 const hamburger = document.querySelector(".hamburger")
 const navMenu = document.querySelector(".navMenu")
+const header = document.querySelector("header")
 
-hamburger.addEventListener("click", () =>{
+hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active")
   navMenu.classList.toggle("active")
 })
 
 document.querySelectorAll(".navLink").forEach(n => n.
-  addEventListener("click", () =>{
+  addEventListener("click", () => {
     hamburger.classList.remove("active")
     navMenu.classList.remove("active")
   }))
+
+// slider function//
+let count = 1
+document.getElementById("animal1").checked = true
+
+setInterval(function () {
+  nextSlide()
+}, 4000)
+
+function nextSlide() {
+  count++
+  if (count > 3) {
+    count = 1
+  }
+  document.getElementById("animal" + count).checked = true
+}
+
+// header scroll function//
+window.addEventListener("scroll", () =>{
+  header.classList.toggle("scrolled", window.scrollY > 0)
+  hamburger.classList.toggle("scrolled", window.scrollY > 0)
+  navMenu.classList.toggle("scrolled", window.scrollY > 0)
+})
